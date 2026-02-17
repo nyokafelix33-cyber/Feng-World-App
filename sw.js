e7e5be450ddd62a94e892fe4759addbebcf6bda4
Felix Nyoka <nyokafelix33@gmail.com>
2026-02-17 12:33:54 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,38 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'feng-world-cache-v1';
+const ASSETS = [
+  '/index.html',
+  '/manifest.json',
+  '/style.css', // if you have a separate CSS file
+  '/app.js',    // if you move JS outside HTML
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
+  'https://i.ibb.co/4tB3K3H/feng-logo.png'
+];
+
+// Install service worker & cache assets
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate service worker & clean old caches
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+});
+
+// Fetch event: serve cached assets if offline
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(response => response || fetch(e.request))
+      .catch(() => caches.match('/index.html'))
+  );
+});
